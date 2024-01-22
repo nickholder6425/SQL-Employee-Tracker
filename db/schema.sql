2bb6b270d0402fd86028ba3dba5c38ab04671dc3
nicholasholder <ngholder@hotmail.com>
2024-01-22 13:24:53 -0600

    modified db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,26 @@
-ERROR: 
-Failed to open file 'DB/SHEMEA.SQL', error: 2
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL DEFAULT 0,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(75) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT DEFAULT NULL REFERENCES employee(id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
